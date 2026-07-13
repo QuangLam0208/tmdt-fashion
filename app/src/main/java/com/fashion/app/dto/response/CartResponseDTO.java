5851318221d04b7305ee5c14545b8f52156dbf74
Luong Quang Lam <laulua1223@gmail.com>
2026-07-13 23:22:49 +0700

    feat: giỏ hàng (thêm/sửa/xoá + kiểm tra tồn kho)

---
+++ app/src/main/java/com/fashion/app/dto/response/CartResponseDTO.java
@@ -0,0 +1,17 @@
+package com.fashion.app.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartResponseDTO {
+    private List<CartItemResponseDTO> items;
+    private Double totalAmount;
+}
